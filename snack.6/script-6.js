cf24c1316256bc6a901d646605b540055376f240
MarcoFonti <theemporiumg@gmail.com>
2024-01-04 15:51:02 +0100

    ex 6 snack

---
+++ snack.6/script-6.js
@@ -0,0 +1,13 @@
+// Check
+console.log('JS OK SNACK 6');
+
+//! SCALETTA
+// 1 Chiediamo all'utente un numero 
+// 2 Creiamo la somma di un cubo
+// 3 Stampo
+
+const numberUser = parseInt(prompt('Dimmi un numero'));
+
+const totNumber = numberUser ** 3
+
+console.log(totNumber);
